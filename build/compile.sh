37d776b4d435054ce9bc7553c92d8e70b06eac8d
Himanshu Gautam <1302399+hgautam@users.noreply.github.com>
2020-08-28 15:57:48 -0700

    Create compile.sh

---
+++ build/compile.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# This script compiles all .md files into a single md file to converted into docx file
+# This script will normalize .md files by removing Hugo specifc details from those files
+output="../content/home/output.md"
+content_path="../content/home"
+# this file will be pre-generated in previous make target
+input="files.txt"
+while IFS= read -r file
+do
+  echo "compiling $file..."
+  sed 1,3d "$content_path/$file" >> "$output"
+  # add couple of empty lines at the end of the file
+  echo >> "$output"
+  echo >> "$output"
+  # add --- separator at the end of the file
+  echo --- >> "$output"
+  # add one more empty line at the end of  the file
+  echo >> "$output"
+done < "$input"
+echo "converting into pdf.."
+cd "$content_path"
+markdown-pdf output.md
+#pandoc -o output.docx output.md
